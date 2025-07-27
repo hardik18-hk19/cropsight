85bc0019495d8945184e0796f95ecfac954565f3
Manyathaaa <mailtomanyatha@gmail.com>
2025-07-27 18:09:24 +0530

    feat: Add PredictPriceController and route for price prediction

--- server/routes/vendorRoutes.js
+++ server/routes/vendorRoutes.js
@@ -1,5 +1,10 @@
 import express from "express";
+import { PredictPriceController } from "../controllers/vendorController.js";
 
 const router = express.Router();
+
+//price predict
+
+router.post("/predict-price", PredictPriceController);
 
 export default router;
